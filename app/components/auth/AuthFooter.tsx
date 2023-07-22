31131b629ddb03bd171c5366335321bbc624244f
Lucas <lapmrj@gmail.com>
2023-07-21 23:15:32 -0300

    fix auth ui problems

--- app/components/auth/AuthFooter.tsx
+++ app/components/auth/AuthFooter.tsx
@@ -7,7 +7,7 @@ export default function AuthFooter({isSignIn}: {isSignIn: boolean}){
       <div className={`flex justify-center ${isSignIn && 'mb-3'} `}>
         <AiFillGoogleCircle className="h-8 w-8 cursor-pointer hover:text-gray-800 transition duration-200"/>
       </div>
-      <p className="text-center ">{isSignIn ? 'Não tem uma conta?' : 'Já tem uma conta?'}<br/>
+      <p className="text-center ">{isSignIn ? 'Não tem uma conta? ' : 'Já tem uma conta? '}<br className="sm:inline hidden"/>
         <Link href={isSignIn ? '/signUp' : '/signIn'} className={`font-bold mt-2 hover:text-gray-800 transition duration-200`}>
         {isSignIn ? 'Cadastre-se.' : 'Entre'}
         </Link> 
--- app/components/auth/AuthForm.tsx
+++ app/components/auth/AuthForm.tsx
@@ -1,19 +1,41 @@
+"use client"
+
 import { BsLock, BsPersonCircle } from "react-icons/bs";
-import StyledButton from "../styledButton/StyledButton";
-import StyledInput from "../styledInput/StyledInput";
+import StyledButton from "../utility/styledButton/StyledButton";
+import StyledInput from "../utility/styledInput/StyledInput";
 import { AiOutlineMail } from "react-icons/ai";
+import {useForm} from 'react-hook-form'
+import {zodResolver} from '@hookform/resolvers/zod'
+import { SignInType, SignUpType, signInValidation, signUpValidation } from "./validateAuth";
+import { useState } from "react";
+import ErrorMessage from "../utility/errorMessage/ErrorMessage";
 
 export default function AuthForm({isSignIn}: {isSignIn: boolean}){
+  const [output, setOutput] = useState()
+
+  const {register, handleSubmit, formState: {errors}} = useForm<typeof isSignIn extends true ? SignInType : SignUpType>({
+    resolver: zodResolver(isSignIn ? signInValidation : signUpValidation)
+  })
+  const validate = (data: any) => {
+    setOutput(data)
+  }
   return <>
-    <form action="" className=" mt-4 flex flex-col gap-6 ">
+    <form action="" onSubmit={handleSubmit(validate)} className=" mt-4 flex flex-col gap-6 ">
             <div className="space-y-2 w-full">
-              { !isSignIn && 
-                <StyledInput id="email" Icon={AiOutlineMail}/>
+              { !isSignIn && <>
+                {errors.email?.message && <ErrorMessage message={errors.email.message}/>}
+                <StyledInput id="email" Icon={AiOutlineMail} register={register} name="email" placeholder="Email"/>
+              </>
               }
-                <StyledInput id="username" Icon={BsPersonCircle}/>
-                <StyledInput type="password" id="password" Icon={BsLock}/>
-              {!isSignIn &&
-                <StyledInput type="password" id="password" Icon={BsLock}/>
+                {errors.username?.message && <ErrorMessage message={errors.username.message}/>}
+                <StyledInput id="username" Icon={BsPersonCircle} register={register} name="username" placeholder="Nome de usuario"/>
+                
+                {errors.password?.message && <ErrorMessage message={errors.password.message}/>}
+                <StyledInput type="password" id="password" Icon={BsLock} register={register} name="password" placeholder="Senha"/>
+              {!isSignIn &&<>
+                {errors.repeatPassword?.message && <ErrorMessage message={errors.repeatPassword.message}/>}
+                <StyledInput type="password" id="repeatPassword" Icon={BsLock} register={register} name="repeatPassword" placeholder="Repetir a senha"/>
+              </>
               }
             </div>
             <StyledButton text={isSignIn ? 'Entrar': 'Criar'}/>
